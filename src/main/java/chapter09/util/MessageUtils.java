5632a26aff757d5ab374f352e3516fc2c6137028
mayanhui <binma85@gmail.com>
2014-11-28 13:47:47 +0800

    add chapter09 source code

---
+++ src/main/java/chapter09/util/MessageUtils.java
@@ -0,0 +1,22 @@
+package chapter09.util;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+import kafka.message.Message;
+
+public class MessageUtils {
+	public static final String DEFAULT_ENCODING = "utf8";
+
+	public static String getMessage(Message message) {
+		ByteBuffer buffer = message.payload();
+		byte[] bytes = new byte[buffer.remaining()];
+		buffer.get(bytes);
+		try {
+			return new String(bytes, DEFAULT_ENCODING);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
