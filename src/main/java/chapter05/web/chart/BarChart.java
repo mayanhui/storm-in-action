dbbb36ca0f2a67c83be71bb7dff025e7bdae35a9
mayanhui <binma85@gmail.com>
2014-12-13 21:04:06 +0800

    fix

--- src/main/java/chapter05/web/chart/BarChart.java
+++ src/main/java/chapter05/web/chart/BarChart.java
@@ -2,8 +2,12 @@ package chapter05.web.chart;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,76 +15,85 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.DateTickMarkPosition;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.servlet.ServletUtilities;
 import org.jfree.chart.title.TextTitle;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.time.Year;
-import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import chapter05.web.model.DataSet;
+import chapter05.web.util.JDBCUtils;
 
 public class BarChart {
-	private static IntervalXYDataset createDataset(){
-		TimeSeriesCollection datase=new TimeSeriesCollection();
-		TimeSeries series=new TimeSeries("������",Year.class);
-		series.add(new Year(2000),5000);
-		series.add(new Year(2001),5500);
-		series.add(new Year(2002),6000);
-		series.add(new Year(2003),8000);
-		series.add(new Year(2004),7000);
-		series.add(new Year(2005),7500);
-		series.add(new Year(2006),6000);
-		series.add(new Year(2007),12000);
-		series.add(new Year(2008),8000);
-		datase.addSeries(series);
-		return datase;
+	private static Map<Integer, CategoryDataset> createDataset() {
+
+		Map<Integer, List<DataSet>> dataMap = JDBCUtils.getData(new Date());
+		Map<Integer, CategoryDataset> cdsMap = new TreeMap<Integer, CategoryDataset>();
+		DefaultCategoryDataset cgd = null;
+		for (Integer i : dataMap.keySet()) {
+			cgd = new DefaultCategoryDataset();
+			for (DataSet ds : dataMap.get(i)) {
+				cgd.addValue(ds.getCount(), "异常数目", ds.getTimes() + "");
+			}
+			cdsMap.put(i, cgd);
+		}
+		return cdsMap;
 	}
-	
-	private static JFreeChart createChart(IntervalXYDataset xyDataSet){
-		JFreeChart jFreeChart=ChartFactory.createXYBarChart(
-				"���꾭��ծ�ķ�ծ������",				//����
-				"��", 							//x���ǩ
-				true,							//�Ƿ���ʾ����
-				"���/��Ԫ",						//y���ǩ
-				xyDataSet,						//��ݼ�
-				PlotOrientation.VERTICAL,		//��ʾ����
-				true,							//�Ƿ���ʾͼ��
-				false,							//�Ƿ��й�����
-				false);							//�Ƿ�������
+
+	public static void main(String[] args) {
+		createDataset();
+	}
+
+	private static JFreeChart createChart(CategoryDataset xyDataSet) {
+
+		JFreeChart jFreeChart = ChartFactory.createBarChart("异常账户统计报告", // 标题
+				"统计次数", // x轴标签 //是否显示日期
+				"异常帐号个数/个", // y轴标签
+				xyDataSet, // 数据集
+				PlotOrientation.VERTICAL, // 显示方向
+				true, // 是否显示图例
+				false, // 是否有工具条
+				false); // 是否有链接
 		jFreeChart.setBackgroundPaint(Color.white);
-		jFreeChart.addSubtitle(new TextTitle("������ҵ����",new Font("Dialog",Font.ITALIC,10)));
-		XYPlot plot=(XYPlot) jFreeChart.getPlot();
-		XYBarRenderer renderer=(XYBarRenderer) plot.getRenderer();
-		renderer.setMargin(0.50);
-		DateAxis axis=(DateAxis) plot.getDomainAxis();
-		axis.setTickMarkPosition(DateTickMarkPosition.MIDDLE);
-		axis.setLowerMargin(0.01);
-		axis.setUpperMargin(0.01);
+		jFreeChart.addSubtitle(new TextTitle("异常帐号报表", new Font("Dialog",
+				Font.ITALIC, 10)));
+		CategoryPlot cplot = (CategoryPlot) jFreeChart.getPlot();
+		BarRenderer render = (BarRenderer) cplot.getRenderer();
+		render.setItemMargin(0.10);
+		render.setMaximumBarWidth(0.05);
+		// render.set
+		CategoryAxis axis = cplot.getDomainAxis();
+		axis.setLowerMargin(0.1);
+		axis.setUpperMargin(0.1);
 		return jFreeChart;
-		
-		
+
 	}
-	
-	public static String generateBarChart(HttpSession session,PrintWriter pw){
-		String filename=null;
-		IntervalXYDataset xyDataset=createDataset();
-		JFreeChart chart=createChart(xyDataset);
-		ChartRenderingInfo info=new ChartRenderingInfo();
+
+	public static void generateBarChart(HttpSession session, PrintWriter pw) {
+
+		List<String> fileNames = new ArrayList<String>();
 		try {
-			filename=ServletUtilities.saveChartAsPNG(chart, 500,300, info, session);
-			ChartUtilities.writeImageMap(pw, filename, info, false);
-			pw.flush();
+			String filename = null;
+			Map<Integer, CategoryDataset> datasets = createDataset();
+			for (Integer i : datasets.keySet()) {
+				JFreeChart chart = createChart(datasets.get(i));
+				ChartRenderingInfo info = new ChartRenderingInfo();
+				filename = ServletUtilities.saveChartAsPNG(chart, 500, 300,
+						info, session);
+				fileNames.add(filename);
+				System.out.println(filename);
+				ChartUtilities.writeImageMap(pw, filename, info, false);
+				pw.flush();
+			}
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return filename;
+		session.setAttribute("filenames", fileNames);
+
 	}
-	
-	
-	
+
 }
--- src/main/java/chapter05/web/model/DataSet.java
+++ src/main/java/chapter05/web/model/DataSet.java
@@ -3,18 +3,21 @@ package chapter05.web.model;
 public class DataSet {
 	private int times;
 	private int count;
+
 	public int getTimes() {
 		return times;
 	}
+
 	public void setTimes(int times) {
 		this.times = times;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
-	
-	
+
 }
