2a18e20900c5927bc5027b9d916599af2fecb454
mayanhui <binma85@gmail.com>
2014-12-25 16:24:41 +0800

    add sample data

--- src/main/java/chapter06/datasource/MakeData.java
+++ src/main/java/chapter06/datasource/MakeData.java
@@ -36,12 +36,8 @@ class parseTime {
 
 public class MakeData {
 
-	/**
-	 * @param args
-	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		BufferedReader br = null;
 		BufferedWriter bw = null;
 		int count = 0, index = 0, carSign = 0, speed = 0, bearing = 0, occupied = 0, fileNum = 0, extSecond = 0;
@@ -50,23 +46,21 @@ public class MakeData {
 		DateFormat format = new SimpleDateFormat("2014-02-08 17:29:58");
 		String time = format.format(date_time);
 		parseTime pt = new parseTime();
-		String carNumstr = "", readRecordLine =null;
+		String carNumstr = "", readRecordLine = null;
 		char[] str = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 		String record = null;
-		// br=new BufferedReader(new FileReader(filepath));
 		Random random = new Random();
 		Set<String> carNum = new HashSet<String>();
-		for (; fileNum < 5; fileNum++) {
+		for (; fileNum < 2; fileNum++) {
 			try {
-				bw = new BufferedWriter(new FileWriter("E:\\GPSData\\"
+				bw = new BufferedWriter(new FileWriter("GPSData-"
 						+ (fileNum + 1) + ".txt"));
-				File file=new File("E:\\GPSData\\"
-						+ fileNum + ".txt");
-				if(file.exists()){
-					br = new BufferedReader(new FileReader("E:\\GPSData\\"
-						+ fileNum + ".txt"));
+				File file = new File("E:\\GPSData\\" + fileNum + ".txt");
+				if (file.exists()) {
+					br = new BufferedReader(new FileReader("GPSData-" + fileNum
+							+ ".txt"));
 					readRecordLine = br.readLine();
-					while(readRecordLine!=null){
+					while (readRecordLine != null) {
 						String[] recordLine = readRecordLine.split(",");
 						extLo = random.nextDouble() / 100;
 						extLa = random.nextDouble() / 100;
@@ -77,25 +71,25 @@ public class MakeData {
 						String newTime = pt.add(recordLine[3], 30);
 						speed = random.nextInt(71);
 						bearing = random.nextInt(361);
-						if(speed==0){
-							occupied =1;
-						}else{
-							occupied=0;
+						if (speed == 0) {
+							occupied = 1;
+						} else {
+							occupied = 0;
 						}
-						
-						record = recordLine[0] + "," + String.format("%.6f", newlongitude) + ","
-								+ String.format("%.6f", newlantitude) + "," + newTime + "," + speed + ","
-								+ bearing + "," + occupied;
+
+						record = recordLine[0] + ","
+								+ String.format("%.6f", newlongitude) + ","
+								+ String.format("%.6f", newlantitude) + ","
+								+ newTime + "," + speed + "," + bearing + ","
+								+ occupied;
 						readRecordLine = br.readLine();
 						bw.write(record);
 						bw.newLine();
 						bw.flush();
 					}
-					
-					
-				}
-				else if (readRecordLine == null) {
-					for (; carNum.size() < 50;) {
+
+				} else if (readRecordLine == null) {
+					for (; carNum.size() < 300000;) {
 						carSign = random.nextInt(90) % 26 + 65;
 						carNumstr = (char) carSign + "";
 						while (count < 5) {
@@ -109,35 +103,33 @@ public class MakeData {
 					}
 					for (String s : carNum) {
 						longitude = random.nextDouble() + 117;
-						int base=random.nextInt(17)+1;
 						lantitude = random.nextDouble() + 40;
 						extSecond = random.nextInt(3);
 						time = pt.add(time, extSecond);
 						speed = random.nextInt(71);
 						bearing = random.nextInt(361);
-						if(speed==0){
-							occupied =1;
-						}else{
-							occupied=0;
+						if (speed == 0) {
+							occupied = 1;
+						} else {
+							occupied = 0;
 						}
-						
-						record = "热" + s + "," + String.format("%.6f", longitude)  + "," + String.format("%.6f", lantitude)
-								+ "," + time + "," + speed + "," + bearing
-								+ "," + occupied;
+
+						record = "热" + s + ","
+								+ String.format("%.6f", longitude) + ","
+								+ String.format("%.6f", lantitude) + "," + time
+								+ "," + speed + "," + bearing + "," + occupied;
 						bw.write(record);
 						bw.newLine();
 						bw.flush();
 					}
-				} 
-				
-				
+				}
+
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 			// System.out.println(record);
-			
+
 		}
 	}
 
